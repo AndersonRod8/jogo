594b77348b7200b86384a17ffbe75cb317ba081e
MariaEMadeira <92498850+MariaEMadeira@users.noreply.github.com>
2026-06-12 23:08:35 -0300

    Atualiza sistema de pontuação e condição de vitória
    Refactor comments for clarity and consistency.

--- src/jogo.py
+++ src/jogo.py
@@ -2,21 +2,19 @@
 import pygame
 from src.funcoes import ALTURA
 
-#Movimento da bola e verificação das colisões com as bordas e barras.
-
 def mover_bola(x, y, vel_x, vel_y, tamanho, p1_rect, p2_rect):
-    
+    """Movimenta a bola e verifica colisões com as bordas e barras."""
     x += vel_x
     y += vel_y
 
-    # se a bola bater no topo ou na base
+    # Colisão com o topo ou base
     if y <= 0 or y >= ALTURA - tamanho:
         vel_y *= -1
 
-    # dimensôes da bola
+    # Cria o retângulo da bola para verificar intersecção
     bola_rect = pygame.Rect(x, y, tamanho, tamanho)
 
-    # Colisão da bola com a barra
+    # Colisão com as barras
     if bola_rect.colliderect(p1_rect) or bola_rect.colliderect(p2_rect):
         vel_x *= -1
 
